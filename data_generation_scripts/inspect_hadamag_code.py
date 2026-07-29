c9632ea297806dab5e4f14262640b0e393a73746
Naga Teja <nagateja95@gmail.com>
2026-07-29 13:14:33 +0200

    chore: auto-backup changes and simulation data

--- data_generation_scripts/inspect_hadamag_code.py
+++ data_generation_scripts/inspect_hadamag_code.py
@@ -1,13 +1,9 @@
 import numpy as np
 from juliacall import Main as jl
 
-jl.seval("using HadaMAG")
+jl.seval("using InteractiveUtils; using HadaMAG")
 jl.seval("""
-println("=== HadaMAG SRE Source Code Inspection ===")
-try
-    # Inspect SRE definition in HadaMAG
-    display(@which SRE(HadaMAG.StateVec(ones(ComplexF64, 128), 7, 128), 2))
-catch e
-    println(e)
-end
+println("=== HadaMAG SRE Source Code Location ===")
+m = @which SRE(HadaMAG.StateVec(ones(ComplexF64, 128), 7, 128), 2)
+println(m)
 """)
